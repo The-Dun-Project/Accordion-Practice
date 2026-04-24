fe04ed603c0c837a66f637ec182c22fe97d8209c
THe-Dun-Project <danielgichiadev@gmail.com>
2026-04-24 03:05:24 +0300

    Day 7 - Completed HTML - CSS and 50% of JS without checking

---
+++ day-7/script.js
@@ -0,0 +1,12 @@
+// Pull all accordion items
+// everything happens after we click on the header
+//pull the headers from each item
+// see about the click
+//set the active class
+
+const accordionItems = document.querySelectorAll(".accordion-item");
+
+accordionItems.forEach((item) => {
+    const header = item.querySelector(".accordion-header");
+    const content = item.querySelector(".accordion-content");
+})
