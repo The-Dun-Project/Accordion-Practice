a4fe41eade6993d8da09476b6edfb4bdd800ff79
THe-Dun-Project <danielgichiadev@gmail.com>
2026-04-25 07:59:20 +0300

    Day 8 - Got stuck in JS inside the header click event

---
+++ Day-8/script.js
@@ -0,0 +1,9 @@
+// pull all accordion items
+// loop throgh them to get the header and content
+// click the header to activate fanction
+    // check if others have active
+    // remove them
+    // loop to get the header again "i think"
+
+const allAccordionItems = document.querySelectorAll(".accordion-item");
+
