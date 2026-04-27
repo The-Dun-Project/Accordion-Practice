910da409caa098a910a1e8d786324947ac9620e8
THe-Dun-Project <danielgichiadev@gmail.com>
2026-04-27 03:07:38 +0300

    Day 8 complete

--- Day-8/script.js
+++ Day-8/script.js
@@ -7,3 +7,29 @@
 
 const allAccordionItems = document.querySelectorAll(".accordion-item");
 
+allAccordionItems.forEach((item) => {
+
+    const header = item.querySelector(".accordion-header");
+    const content = item.querySelector(".accordion-content");
+
+    header.addEventListener("click", () => {
+        const isActive = item.classList.contains("active");
+
+        allAccordionItems.forEach((otherItem) => {
+            otherItem.classList.remove("active");
+
+            const otherContent = otherItem.querySelector(".accordion-content");
+            otherContent.style.maxHeight = null;
+        });
+
+        if(!isActive){
+            item.classList.add("active");
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+
+    })
+
+
+
+
+})
