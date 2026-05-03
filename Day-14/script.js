2ed73992848ac62cc6aacbacebe673ee695720c3
THe-Dun-Project <danielgichiadev@gmail.com>
2026-05-03 04:54:11 +0300

    Day 14 - Completed with alot of challenges

--- Day-14/script.js
+++ Day-14/script.js
@@ -1,4 +1,4 @@
-const allAccordionItems = document.querySelector(".accordion-item");
+const allAccordionItems = document.querySelectorAll(".accordion-item");
 
 allAccordionItems.forEach((item) => {
     const header = item.querySelector(".accordion-header");
@@ -10,8 +10,13 @@ allAccordionItems.forEach((item) => {
         allAccordionItems.forEach((otherItem) => {
             otherItem.classList.remove("active");
 
-            const otherContent = querySelector(".accordion-content");
+            const otherContent = otherItem.querySelector(".accordion-content");
             otherContent.style.maxHeight = null;
         })
-    });
+
+        if(!isActive){
+            item.classList.add("active");
+            content.style.maxHeight = item.scrollHeight + "px";
+        }
+    })
 })
